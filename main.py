7810e55b9917e06bfdc1076bf297d29a1aef67d1
Stoica Tedy <stoicatedy@gmail.com>
2020-06-18 08:09:12 +0300

    uuid to name

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import HypixelMonitor.config as config
 
 # https://api.hypixel.net/player?key=&name=Tedyst
 
-HYPIXEL_EXP = Gauge('hypixel_exp', 'Network EXP', ["uuid"])
-SKYWARS_SOULS = Gauge('hypixel_skywars_souls', 'SkyWars Souls', ["uuid"])
+HYPIXEL_EXP = Gauge('hypixel_exp', 'Network EXP', ["name"])
+SKYWARS_SOULS = Gauge('hypixel_skywars_souls', 'SkyWars Souls', ["name"])
 SKYWARS_WIN_STREAK = Gauge('hypixel_skywars_win_streak',
-                           'SkyWars Win Streak', ["uuid"])
-BEDWARS_EXP = Gauge('hypixel_bedwars_exp', 'BedWars EXP', ["uuid"])
+                           'SkyWars Win Streak', ["name"])
+BEDWARS_EXP = Gauge('hypixel_bedwars_exp', 'BedWars EXP', ["name"])
 BEDWARS_WIN_STREAK = Gauge('hypixel_bedwars_win_streak',
-                           'BedWars Win Streak', ["uuid"])
-BEDWARS_COINS = Gauge('hypixel_bedwars_coins', 'BedWars Coins', ["uuid"])
-KARMA = Gauge('hypixel_karma', 'Karma', ["uuid"])
+                           'BedWars Win Streak', ["name"])
+BEDWARS_COINS = Gauge('hypixel_bedwars_coins', 'BedWars Coins', ["name"])
+KARMA = Gauge('hypixel_karma', 'Karma', ["name"])
 
 
 def getUser(uuid):
@@ -24,22 +24,23 @@ def getUser(uuid):
     }
     r = requests.get("https://api.hypixel.net/player", params=params)
     result = r.json()
+    name = result["player"]["displayname"]
 
     xp = result["player"]["networkExp"]
-    HYPIXEL_EXP.labels(uuid).set(xp)
+    HYPIXEL_EXP.labels(name).set(xp)
 
     skywars_souls = result["player"]["stats"]["SkyWars"]["souls"]
-    SKYWARS_SOULS.labels(uuid).set(skywars_souls)
+    SKYWARS_SOULS.labels(name).set(skywars_souls)
     skywars_win_streak = result["player"]["stats"]["SkyWars"]["win_streak"]
-    SKYWARS_WIN_STREAK.labels(uuid).set(skywars_win_streak)
+    SKYWARS_WIN_STREAK.labels(name).set(skywars_win_streak)
     bedwars_exp = result["player"]["stats"]["Bedwars"]["Experience"]
-    BEDWARS_EXP.labels(uuid).set(bedwars_exp)
+    BEDWARS_EXP.labels(name).set(bedwars_exp)
     bedwars_win_streak = result["player"]["stats"]["Bedwars"]["winstreak"]
-    BEDWARS_WIN_STREAK.labels(uuid).set(bedwars_win_streak)
+    BEDWARS_WIN_STREAK.labels(name).set(bedwars_win_streak)
     bedwars_coins = result["player"]["stats"]["Bedwars"]["coins"]
-    BEDWARS_COINS.labels(uuid).set(bedwars_coins)
+    BEDWARS_COINS.labels(name).set(bedwars_coins)
     karma = result["player"]["karma"]
-    KARMA.labels(uuid).set(karma)
+    KARMA.labels(name).set(karma)
 
 
 if __name__ == '__main__':
